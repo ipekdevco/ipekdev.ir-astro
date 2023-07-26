667a8e463ab6085d16c3de7af8eec7359c1119db
Farzad Faal <farzadfaal@outlook.com>
2023-07-26 12:01:10 +0330

    initial project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx(), image(), sitemap(), react()]
+});
